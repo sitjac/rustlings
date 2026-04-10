66b9ec5587e8df71e1297613d4a8a1caf192fd8e
sitJac <xiejunjie914@gmail.com>
2026-04-11 00:25:42 +0800

    fix: enhance vector creation

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -1,15 +1,19 @@
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // Array
+    let a: [i32; 4] = [10, 20, 30, 40]; // Array
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
+
+    let v: Vec<i32> = vec![10, 20, 30, 40]; // Vector
 
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+    let (a, v) = array_and_vec();
+    println!("Array: {a:?}, Vector: {v:?}");
 }
 
 #[cfg(test)]
