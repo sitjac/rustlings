db3ad321aa315b392abb57401487b84e1d81c683
sitJac <xiejunjie914@gmail.com>
2026-04-18 14:34:59 +0800

    fix: resolve Clippy lints in exercises

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -1,8 +1,12 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    // TODO: Fix the Clippy lint.
-    for x in option {
+    // let option: Option<i32> = None;
+    
+    // Fix the Clippy lint.
+    // cargo clippy --bin clippy2
+    // warning: for loop over an `Option`. This is more readably written as an `if let` statement
+    if let Some(x) = option {
         res += x;
     }
 
